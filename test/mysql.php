12107a2e98e89b39785b1aa6ad08d6874a9396ed
sy-records <52o@qq52o.cn>
2019-06-13 18:23:47 +0800

    test code

---
+++ test/mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+	function db(){
+		return mysqli_connect("localhost","root","root");
+	}
+	for ($i=0; $i < 10000; $i++) {
+		$db_con  = "db{$i}";
+		$$db_con = db();
+	}
+
---
+++ test/objTest.php
@@ -0,0 +1,47 @@
+<?php
+
+class objTest
+{
+    protected $num;
+    
+    public function __construct($num)
+    {
+        $this->num = $num;
+    }
+
+    public function setNum($num)
+    {
+        $this->num = $num;
+    }
+
+    public function getNum()
+    {
+        return $this->num;
+    }
+}
+
+function microtimeFloat()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return ((float)$usec + (float)$sec);
+}
+
+$startTime = microtimeFloat();
+
+for ($i = 0; $i < 10000; $i++) {
+    $test = new objTest($i);
+    $test->getNum();
+}
+echo "对象不复用耗时" . (microtimeFloat() - $startTime) . '秒' . PHP_EOL;
+
+$startTime = microtimeFloat();
+
+$test = new objTest(0);
+
+for ($i = 0; $i < 10000; $i++) {
+    $test->setNum($i);
+    $test->getNum();
+}
+echo "对象复用耗时" . (microtimeFloat() - $startTime) . '秒' . PHP_EOL;
+
+
