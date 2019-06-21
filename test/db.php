f7819887c3abc78dc26f7e470fd5e6917dc2d971
sy-records <52o@qq52o.cn>
2019-06-21 13:56:46 +0800

    1

--- test/db.php
+++ test/db.php
@@ -9,7 +9,7 @@ $sql = 'select * from wp_users limit 1';
 $db = new DbHelper;
 
 for ($i=0; $i < 10; $i++) {
-	echo '获取第'.$i.'次连接'.PHP_EOL;
-	$res = $db->queryAll($sql);
-//	var_dump($res) . PHP_EOL;
+    echo '获取第'.$i.'次连接'.PHP_EOL;
+    $res = $db->queryAll($sql);
+//  var_dump($res) . PHP_EOL;
 }
